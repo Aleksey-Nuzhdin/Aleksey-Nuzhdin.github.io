74010ca739126c70aa2ca333715dc70a2b813e59
alexNI <alekseynuzdin@gmail.com>
2020-06-17 20:43:34 +0700

    parallax hero v0.5(js)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("load", function(event) {
   
   let preloader = new Vue({
@@ -17,3 +18,35 @@ window.addEventListener("load", function(event) {
   })
 
 });
+
+//parallax
+
+let parallxConteiner = document.getElementById('parallax');
+    section = document.getElementById('section'),
+    parallaxLevels = parallxConteiner.childNodes;
+
+function moveLayers(e){
+  let moveX = (window.innerWidth / 2) - e.pageX,
+      moveY = (window.innerHeight / 2)- e.pageY
+      ;
+  
+  if(window.innerHeight > e.pageY){
+
+    for(let i = 1; i < 4; i++){
+      let coefficent,
+          levelStyle;
+
+      levelStyle = parallaxLevels[i].style;
+      coefficent =  window.innerWidth/(25 * i);
+      levelStyle.transform = 'translate3d(' + (moveX/coefficent) + 'px ,' + (moveY/coefficent)/2 + 'px , 0)';
+    }
+  }
+
+};
+
+
+section.addEventListener('mousemove', moveLayers);
+
+
+
+
